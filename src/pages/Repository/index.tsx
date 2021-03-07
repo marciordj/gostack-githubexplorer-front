e2c45211e55233f41e8e70d86563644f07c7877e
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2021-03-07 16:36:46 -0300

    checkpoint

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,4 +1,10 @@
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi'
+
+import logoImage from '../../assets/githubexplorer_logo.svg';
+
+import { Header, RepositoryInfo } from './styles';
+import { Fragment } from 'react';
 
 interface IRepositoryParams {
   repository: string;
@@ -8,7 +14,19 @@ const Repository = () => {
   const { params } = useRouteMatch<IRepositoryParams>();
 
   return (
-    <h1>Repo: {params.repository}</h1>
+    <Fragment>
+      <Header>
+        <img src={logoImage} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header></header>
+      </RepositoryInfo>
+    </Fragment>
   )
 };
 
