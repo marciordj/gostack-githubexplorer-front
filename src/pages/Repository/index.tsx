10205b9d9f62da095770223157f93c6feb3c315c
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2021-05-14 15:43:21 -0300

    feature: add endpoint github

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,10 +1,12 @@
-import { useRouteMatch, Link } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi'
+import { Fragment, useEffect } from "react";
+import { useRouteMatch, Link } from "react-router-dom";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
-import logoImage from '../../assets/githubexplorer_logo.svg';
+import logoImage from "../../assets/githubexplorer_logo.svg";
 
-import { Header, RepositoryInfo } from './styles';
-import { Fragment } from 'react';
+import { Header, Issues, RepositoryInfo } from "./styles";
+
 
 interface IRepositoryParams {
   repository: string;
@@ -13,22 +15,68 @@ interface IRepositoryParams {
 const Repository = () => {
   const { params } = useRouteMatch<IRepositoryParams>();
 
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data.full_name)
+    })
+  }, [params])
+
   return (
     <Fragment>
       <Header>
         <img src={logoImage} alt="Github Explorer" />
-        <Link to="/dashboard">
+        <Link to="/">
           <FiChevronLeft size={16} />
           Voltar
         </Link>
       </Header>
 
       <RepositoryInfo>
-        <header></header>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/69631?v=4"
+            alt="Avatar img"
+					/>
+
+					<div>
+						<strong>{params.repository}</strong>
+						<p>ablue</p>
+					</div>
+
+        </header>
+
+				<ul>
+					<li>
+						<strong>180321</strong>
+						<span>Stars</span>
+					</li>
+
+          <li>
+						<strong>48</strong>
+						<span>Forks</span>
+					</li>
+
+          <li>
+						<strong>67</strong>
+						<span>Issues abertas</span>
+					</li>
+				</ul>
       </RepositoryInfo>
+
+      <Issues>
+        <Link to='dsadsa' >
+
+        <div>
+          <strong>dsaeqw</strong>
+          <p>dsaewqf</p>
+        </div>
+
+        <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </Fragment>
-  )
+  );
 };
 
-
 export default Repository;
+
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
+/*
+  ! /repository/:aqui mostra que o path vai receber um parametro, coloca um +, indica que vai receber mais parametro
+*/
+
 const Routes = () => (
   <Switch>
     <Route path='/' component={Dashboard} exact />
